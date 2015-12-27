56ab876f642b9c6aff9889dd018d008a3b88ffd1
Grégoire RIVIERE <gregoirer42@gmail.com>
2015-12-27 18:31:45 +0100

    Constructeur Game_hdlr

--- hd/game_hdlr.hpp
+++ hd/game_hdlr.hpp
@@ -1,1 +1,12 @@
 #include "..hd/snake.hpp"
+
+class Game_hdlr{
+
+	Snake *m_s;
+	
+	public:
+	
+	Game_hdlr(Snake* s);
+
+
+};
---
+++ src/game_hdlr.cpp
@@ -0,0 +1,5 @@
+#include "game_hdlr.hpp"
+
+Game_hdlr::Game_hdlr(Snake *s){
+	m_s = s;
+}

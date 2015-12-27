2119d5cb0c736d3c00f9b5bb72bc2d183953537e
Grégoire RIVIERE <gregoirer42@gmail.com>
2015-12-27 22:38:40 +0100

    Ajout fichier constante / iteration du game_hdlr

--- hd/game_hdlr.hpp
+++ hd/game_hdlr.hpp
@@ -1,13 +1,16 @@
 #include "..hd/snake.hpp"
 
+
 class Game_hdlr{
 
 	Snake *m_s;
 	sf::RenderWindow *m_w;
+	Dir dir_courante;
+	bool ajout;
 	
 	public:
 	
 	Game_hdlr(Snake* s, sf::RenderWindow *w);
-
+	void iteration();
 
 };
